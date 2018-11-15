0aca741828e1be877249760ad06aa82a23cc7a9d
George Kostenkov <44926005+GeorgeKostenkov@users.noreply.github.com>
2018-11-16 02:37:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
     argc = 1;
-    string amount = argv[1];
-    for (int i = 0; i < stoi(amount); ++i)
+    string times = argv[1];
+    for (int i = 0; i < stoi(times); ++i)
         cout << "Path Planning\n";
     return 0;
 }
