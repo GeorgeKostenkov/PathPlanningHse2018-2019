a06de3b072635508156e0a224025edacaae104de
George Kostenkov <44926005+GeorgeKostenkov@users.noreply.github.com>
2019-01-31 22:39:54 +0300

    Update astar.h

--- astar.h
+++ astar.h
@@ -9,7 +9,7 @@ class Astar : public ISearch
         Astar(double HW, bool BT);
 
     protected:
-        double computeHFromCellToCell(int i1, int j1, int i2, int j2, const EnvironmentOptions &options) override;
+        virtual double computeHFromCellToCell(int i1, int j1, int i2, int j2, const EnvironmentOptions &options);
 };
 
 #endif
