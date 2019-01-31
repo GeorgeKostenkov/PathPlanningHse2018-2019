8d2bf8aec5354488e76e9fb3ed86ceca109d6266
George Kostenkov <44926005+GeorgeKostenkov@users.noreply.github.com>
2019-01-31 22:40:47 +0300

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,3 +2,5 @@
 
 Dijkstra::Dijkstra() : Astar(0, CN_SP_BT_GMAX) {}
 
+double Dijkstra::computeHFromCellToCell(int start_i, int start_j, int fin_i, int fin_j, const EnvironmentOptions &options) {return 0;}
+
