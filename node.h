f4ac3ee620c85f0c3df69b3d5353548e476f440d
George Kostenkov <44926005+GeorgeKostenkov@users.noreply.github.com>
2019-01-31 23:21:51 +0300

    Update node.h

--- node.h
+++ node.h
@@ -16,15 +16,11 @@
 struct Node
 {
     int     i, j; //grid cell coordinates
-    double  F, g{std::numeric_limits<double>::max()}, H; //f-, g- and h-values of the search node
+    double  F, g, H; //f-, g- and h-values of the search node
     Node    *parent; //backpointer to the predecessor node (e.g. the node which g-value was used to set the g-velue of the current node)
 
     bool operator== (const Node &other) const {
         return i == other.i && j == other.j;
-    }
-
-    bool operator< (const Node &other) const {
-        return F < other.F;
     }
 };
 #endif
